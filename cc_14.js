dba6c9b69d98eaa4bfd0fdc9edebea6cd1baa16c
Eugene Rodriguez <er11@usf.edu>
2025-03-09 17:12:58 -0400

    Task 2 - Support Tickets Dynamic Addition

---
+++ cc_14.js
@@ -0,0 +1,39 @@
+// task 2
+function addTicket(customerName, issueDescription, priorityLevel) {
+    const ticketContainer = document.getElementById('ticketContainer');
+
+    // making a div for tickets
+    const ticket = document.createElement('div');
+    ticket.setAttribute('class', 'ticket');
+
+    // section for customer's name
+    const nameHeading = document.createElement('h2');
+    nameHeading.textContent = customerName;
+
+    // section for issue description
+    const issueText= document.createElement('p');
+    issueText.textContent = issueDescription;
+
+    // section for priority level
+    const priorityState = document.createElement('span');
+    priorityState.textContent = `Priority: ${priorityLevel}`;
+    priorityState.setAttribute('class', priorityLevel.toLowerCase());
+
+    // making a button to resolve ticket issue
+    const resolveButton = document.createElement('button');
+    resolveButton.textContent = 'Resolve';
+    resolveButton.setAttributeNS('class', 'resolve-button');
+
+    // section for appending elements to ticket
+    ticket.appendChild(nameHeading);
+    ticket.appendChild(issueText);
+    ticket.appendChild(priorityState);
+    ticket.appendChild(resolveButton);
+    ticketContainer.appendChild(ticket); // appending ticket to container
+}
+
+// examples
+addTicket('Victor Handson', 'Cannot delete account', 'High');
+addTicket('Gerald Jackson', 'Page not showing', 'Low');
+
+
